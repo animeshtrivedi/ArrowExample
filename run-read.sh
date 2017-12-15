62f74c701dec07bab5026cda57503555c0fd03b0
Animesh Trivedi <animesh.trivedi@gmail.com>
2017-12-15 17:49:38 +0100

    add assembling and compiling scripts

---
+++ run-read.sh
@@ -0,0 +1,1 @@
+java -jar ./target/arrowexample-1.0-jar-with-dependencies.jar com.github.animeshtrivedi.arroeexample.ArrowWrite 
---
+++ run-write.sh
@@ -0,0 +1,1 @@
+java -cp ./target/arrowexample-1.0-jar-with-dependencies.jar com.github.animeshtrivedi.arrowexample.ArrowWrite 
